a0ac12935577e25e2c74fcd2d99f8800faf1da1f
KbZo <kevin-10b2011@hotmail.com>
2022-05-11 22:18:25 -0500

    Metodo Con Pamaetro y Variable Parametrizada

--- src/main/java/org/acme/ExampleResource.java
+++ src/main/java/org/acme/ExampleResource.java
@@ -1,18 +1,30 @@
 package org.acme;
 
+import org.eclipse.microprofile.config.inject.ConfigProperties;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class ExampleResource {
+    @ConfigProperty(name="greeting")
+    private String greeting;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from RESTEasy Reactive";
     }
-
     //SI cambio en esta clase tambien se cambia en la clase testHelloEndpoint
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN) //Retornar Texto Plano
+    @Path("custom/{name}")
+    public String customHello(@PathParam("name") String name){
+        return greeting + " Quarkus Framework "+name + " Te amito mucho";
+    }
 }
